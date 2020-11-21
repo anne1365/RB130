e8ed78c90142f67b08b2c87560c4b2d7e4c55806
Anne Jones <anne1365@outlook.com>
2020-11-20 18:04:22 -0800

    Working with blocks

---
+++ L1_Blocks/block_passing_expt.rb
@@ -0,0 +1,44 @@
+# # THIS WORKS
+# def say(words)
+#   yield if block_given?
+#   puts "> " + words
+# end
+
+# say("hi there") do
+#   system 'cls'
+# end
+
+# # THIS DOESN'T
+# def say(words)
+#   yield if block_given?
+#   puts "> " + words
+# end
+
+# say("hi there")
+
+# do
+#   system 'cls'
+# end
+
+
+# does a block have to be preceded by method invocation?
+
+
+
+# def words
+#   yield('these')
+# end
+
+# words do |word1, word2|
+#   puts "WORDS: #{word1} #{word2}"
+# end
+
+# # WORDS: these
+# # => nil
+
+def compare(str)
+  puts "Before: #{str}"
+  puts "After: #{yield(str)}" if block_given?
+end
+
+compare('hi') { |word| word.upcase }
---
+++ L1_Blocks/tempCodeRunnerFile.rb
@@ -0,0 +1,9 @@
+# THIS WORKS
+# def say(words)
+#   yield if block_given?
+#   puts "> " + words
+# end
+
+# say("hi there") do
+#   system 'cls'
+# end
