5094e7223c28b9ed4dd29e2f0f6874b83f7842bb
Anne Jones <anne1365@outlook.com>
2020-11-28 19:26:11 -0800

    octal

---
+++ Ruby_Challenges/easy_1/octal/octal.rb
@@ -0,0 +1,7 @@
+=begin
+- separate number into digits (will reverse # & form array)
+- each index value is the 'power'
+- each with index
+- Find largest power of 8 that is less than # - subtract from #
+- Find next largest / subtract and so on
+=end
