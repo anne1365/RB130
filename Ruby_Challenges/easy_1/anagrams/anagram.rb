d5355701b154608d73eee0eae625807c3d265a9c
Anne <anne1365@outlook.com>
2020-12-04 15:33:04 -0800

    beginning challenged for test prep

---
+++ Ruby_Challenges/easy_1/anagrams/anagram.rb
@@ -0,0 +1,12 @@
+=begin
+PROBLEM: given a word (string) and an array, return the element or
+         elements of the array that are anagrams of the word
+
+  INPUT: string
+  OUTPUT: array
+  RULES:
+   - letters within argument word cannot be reused to form anagram
+   - if there are no anagrams an empty array should be returned
+   - if an array element contains extra letters that don't appear in
+     argument, the element isn't an anagram
+=end
