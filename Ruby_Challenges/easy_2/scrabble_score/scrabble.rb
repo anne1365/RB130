86232a414481dfa8dd9be412e439b03055606590
Anne Jones <anne1365@outlook.com>
2020-12-08 12:41:19 -0800

    Scrabble score

--- Ruby_Challenges/easy_2/scrabble_score/scrabble.rb
+++ Ruby_Challenges/easy_2/scrabble_score/scrabble.rb
@@ -1,26 +1,3 @@
-=begin
- PROBLEM:
-  INPUT:
-  OUTPUT: 
-
-  QUESTIONS:
-
-  RULES:
-   SCORING:
-    Letter                           Value
-    A, E, I, O, U, L, N, R, S, T       1
-    D, G                               2
-    B, C, M, P                         3
-    F, H, V, W, Y                      4
-    K                                  5
-    J, X                               8
-    Q, Z                               10
-
-  DATA STRUCTURE:
-
-  ALGO:
-=end
-
 class Scrabble
   SCORES = {
     1  => %w(A E I O U L N R S T),
@@ -38,7 +15,7 @@ class Scrabble
 
   def score
     return 0 if @word.nil? || !@word.match?(/[a-z]/i)
-    @word.upcase.chars.map { |l| SCORES.select { |_, v| v.include? l }.keys.first }.sum
+    @word.upcase.chars.map { |l| SCORES.select { |_, v| v.include? l } .keys.first } .sum
   end
 
   def self.score(word)
