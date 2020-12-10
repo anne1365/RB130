ffa1eaa08fe7bffa4abaf635741ec5525ff54465
Anne Jones <anne1365@outlook.com>
2020-12-09 19:36:57 -0800

    Beginning odd words

--- Ruby_Challenges/easy_2/odd_words/odd_words.rb
+++ Ruby_Challenges/easy_2/odd_words/odd_words.rb
@@ -1,3 +1,37 @@
 =begin
+CHALLENGE: Consider a character set consisting of letters,
+a space, and a point. Words consist of one or more, but at
+most 20 letters. An input text consists of one or more
+words separated from each other by one or more spaces and
+terminated by 0 or more spaces followed by a point. Input
+should be read from, and including, the first letter of the
+first word, up to and including the terminating point. The
+output text is to be produced such that successive words
+are separated by a single space with the last word being
+terminated by a single point. Odd words are copied in
+reverse order while even words are merely echoed. For
+example, the input string:
 
+"whats the matter with kansas" becomes 
+"whats eht matter htiw kansas."
+
+BONUS POINTS: each character must be read and printed one at
+a time.
+
+------------------------------------------------------------
+PROBLEM: given a string containing letters, spaces, and a
+point, return a copy of the string with 
+
+  INPUT:
+  OUTPUT:
+
+  QUESTIONS:
+
+  RULES:
+
+DATA STRUCTURE:
+
+ALGO:
+
+------------------------------------------------------------
 =end
