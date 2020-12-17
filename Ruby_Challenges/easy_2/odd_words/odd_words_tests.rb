466b641b6d26b8354c48bea9a48efdeb7dab0846
Anne Jones <anne1365@outlook.com>
2020-12-16 19:23:38 -0800

    Rubocopping scrabble

---
+++ Ruby_Challenges/easy_2/odd_words/odd_words_tests.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require_relative 'odd_words'
+
+class OddWordsTests < MiniTest::Test
+  
+end
--- Ruby_Challenges/easy_2/scrabble_score/scrabble.rb
+++ Ruby_Challenges/easy_2/scrabble_score/scrabble.rb
@@ -1,11 +1,11 @@
 class Scrabble
   SCORES = {
-    1  => %w(A E I O U L N R S T),
-    2  => %w(D G),
-    3  => %w(B C M P),
-    4  => %w(F H V W Y),
-    5  => %w(K),
-    8  => %w(J X),
+    1 => %w(A E I O U L N R S T),
+    2 => %w(D G),
+    3 => %w(B C M P),
+    4 => %w(F H V W Y),
+    5 => %w(K),
+    8 => %w(J X),
     10 => %w(Q Z)
   }
 
@@ -15,10 +15,11 @@ class Scrabble
 
   def score
     return 0 if @word.nil? || !@word.match?(/[a-z]/i)
-    @word.upcase.chars.map { |l| SCORES.select { |_, v| v.include? l } .keys.first } .sum
+    word = @word.upcase.chars
+    word.map { |l| SCORES.select { |_, v| v.include? l }.keys.first }.sum
   end
 
   def self.score(word)
-    self.new(word).score
+    new(word).score
   end
 end
