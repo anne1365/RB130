83d65a2c9b70247b0c5674ec8227ea3567a1177b
Anne Jones <anne1365@outlook.com>
2020-12-23 15:50:09 -0800

    Circular buffer

--- Ruby_Challenges/medium_1/circular_buffer/circular_buffer.rb
+++ Ruby_Challenges/medium_1/circular_buffer/circular_buffer.rb
@@ -1,23 +1,34 @@
-class BufferEmptyException < StandardError; end
-
 class CircularBuffer
-  def initialize(size)
-    @size = size
-    @buffer = Array.new(size)
+  def initialize(max_buffer_size)
+    @max_buffer_size = max_buffer_size
+    @buffer = []
   end
 
-  def write(el)
-
+  def write(obj)
+    raise BufferFullException if buffer_full?
+    @buffer << obj unless obj.nil?
   end
 
-  def write!(el)
-
+  def write!(obj)
+    read if buffer_full? unless obj.nil?
+    @buffer << obj unless obj.nil?
   end
 
   def read
-
+    raise BufferEmptyException if @buffer.empty?
+    @buffer.shift
   end
-end
 
-buffer = CircularBuffer.new(4)
-p buffer
+  def clear
+    @buffer.clear
+  end
+
+  private
+
+  def buffer_full?
+    @buffer.size == @max_buffer_size
+  end
+
+  class BufferEmptyException < StandardError; end
+  class BufferFullException < StandardError; end
+end
